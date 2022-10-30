6f850bfbeddc96db34a380d9f43e8f9856665a75
ChirujanoCodding <trdfabris@gmail.com>
2022-10-29 23:21:45 -0300

    removed

--- types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { ChatInputCommandInteraction, EmbedBuilder } from 'discord.js';
-export declare class NavEmbedBuilder {
-    private embeds;
-    private counter;
-    constructor(embeds: EmbedBuilder[]);
-    start(interaction: ChatInputCommandInteraction<'cached'>): Promise<void>;
-}
